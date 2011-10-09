0e7c7d265e134bbdf79fed37f46722f2d5496f51
meh <meh@paranoici.org>
2011-10-09 13:05:08 +0200

    Some style fixes.

--- lib/refining.rb
+++ lib/refining.rb
@@ -11,63 +11,63 @@
 #++
 
 class Object
-  def refine_singleton_method (meth, &block)
-    return unless block
+	def refine_singleton_method (meth, &block)
+		return unless block
 
-    target = self
+		target = self
 
-    (method(meth) rescue nil).tap {|old|
-      old ||= proc {}
+		(method(meth) rescue nil).tap {|old|
+			old ||= proc {}
 
-      what = class << target
-        self
-      end
+			what = class << target
+				self
+			end
 
-      what.__send__ :define_method, meth do |*args, &blk|
-        return if instance_variable_defined?(:@__refining_defined__) && [:method_added, :singleton_method_added].member?(meth)
-        
-        @__refining_defined__ = true
-        what.__send__(:define_method, 'temporary method for refining', &block)
-        
-        target.__send__('temporary method for refining', old.is_a?(UnboundMethod) ? old.bind(self) : old, *args, &blk).tap {
-          what.__send__(:undef_method, 'temporary method for refining') rescue nil
+			what.__send__ :define_method, meth do |*args, &blk|
+				return if instance_variable_defined?(:@__refining_defined__) && [:method_added, :singleton_method_added].member?(meth)
 
-          remove_instance_variable(:@__refining_defined__) rescue nil
-        }
-      end
-    }
-  end
+				@__refining_defined__ = true
+				what.__send__(:define_method, 'temporary method for refining', &block)
+
+				target.__send__('temporary method for refining', old.is_a?(UnboundMethod) ? old.bind(self) : old, *args, &blk).tap {
+					what.__send__(:undef_method, 'temporary method for refining') rescue nil
+
+					remove_instance_variable(:@__refining_defined__) rescue nil
+				}
+			end
+		}
+	end
 end
 
 class Module
-  def refine_method (meth, &block)
-    return unless block
+	def refine_method (meth, &block)
+		return unless block
 
-    (instance_method(meth) rescue nil).tap {|old|
-      old ||= proc {}
+		(instance_method(meth) rescue nil).tap {|old|
+			old ||= proc {}
 
-      define_method(meth) {|*args, &blk|
-        return if instance_variable_defined?(:@__refining_defined__) && [:method_added, :singleton_method_added].member?(meth)
+			define_method(meth) {|*args, &blk|
+				return if instance_variable_defined?(:@__refining_defined__) && [:method_added, :singleton_method_added].member?(meth)
 
-        what = class << self
-          self
-        end
+				what = class << self
+					self
+				end
 
-        @__refining_defined__ = true
-        what.__send__(:define_method, 'temporary method for refining', &block)
-        
-        __send__('temporary method for refining', old.is_a?(UnboundMethod) ? old.bind(self) : old, *args, &blk).tap {
-          what.__send__(:undef_method, 'temporary method for refining') rescue nil
+				@__refining_defined__ = true
+				what.__send__(:define_method, 'temporary method for refining', &block)
 
-          remove_instance_variable(:@__refining_defined__) rescue nil
-        }
-      }
-    }
-  end
+				__send__('temporary method for refining', old.is_a?(UnboundMethod) ? old.bind(self) : old, *args, &blk).tap {
+					what.__send__(:undef_method, 'temporary method for refining') rescue nil
 
-  alias refine_module_method refine_singleton_method
+					remove_instance_variable(:@__refining_defined__) rescue nil
+				}
+			}
+		}
+	end
+
+	alias refine_module_method refine_singleton_method
 end
 
 class Class
-  alias refine_class_method refine_singleton_method
+	alias refine_class_method refine_singleton_method
 end
--- refining.gemspec
+++ refining.gemspec
@@ -1,11 +1,14 @@
 Gem::Specification.new {|s|
-    s.name         = 'refining'
-    s.version      = '0.0.5.2'
-    s.author       = 'meh.'
-    s.email        = 'meh@paranoici.org'
-    s.homepage     = 'http://github.com/meh/refining'
-    s.platform     = Gem::Platform::RUBY
-    s.summary      = 'Easily refine methods'
-    s.files        = Dir.glob('lib/**/*.rb')
-    s.require_path = 'lib'
+	s.name         = 'refining'
+	s.version      = '0.0.5.2'
+	s.author       = 'meh.'
+	s.email        = 'meh@paranoici.org'
+	s.homepage     = 'http://github.com/meh/refining'
+	s.platform     = Gem::Platform::RUBY
+	s.summary      = 'Easily refine methods'
+	s.files        = Dir.glob('lib/**/*.rb')
+	s.require_path = 'lib'
+
+	s.add_development_dependency 'rake'
+	s.add_development_dependency 'rspec'
 }
